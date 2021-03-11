388c4a0e3b47fcf5c591a0fe36e4c964e4750a02
Tony Arthur <tony_arthur@live.com>
2021-03-10 23:38:19 -0300

    Validando CPF Existente

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ app.use(express.json());
 const customers = [];
 app.post('/account', (request, response) => {
     const {cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if(customerAlreadyExists) {
+        return response.status(400).json({ error: 'Customer already exists!' });
+    }
     
     const id = uuidv4();
     customers.push({
